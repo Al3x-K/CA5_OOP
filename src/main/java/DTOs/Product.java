f8ed9f98af6fa3643c965d1d12c098b7b6d6c784
Al3x-K <ola.kail1901@gmail.com>
2024-03-10 17:08:06 +0000

    added product object class

---
+++ src/main/java/DTOs/Product.java
@@ -0,0 +1,53 @@
+package DTOs;
+
+public class Product
+{
+    private int id;
+    private String name;
+
+    public Product(int productID, String name)
+    {
+        this.id = productID;
+        this.name = name;
+    }
+
+    public Product(String name)
+    {
+        this.id = 0;
+        this.name = name;
+    }
+
+    public Product()
+    {
+
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Product {" +
+                "id = " + id +
+                ", name = '" + name + '\'' +
+                '}';
+    }
+}
