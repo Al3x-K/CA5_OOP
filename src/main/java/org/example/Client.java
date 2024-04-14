20b71712d9af7a4174a0ec3d65436c496a373966
Al3x-K <ola.kail1901@gmail.com>
2024-04-14 17:52:30 +0100

    added menu for the client and display by id methods for each entity

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -15,11 +15,76 @@ public class Client
         final int SERVER_PORT = 8888;
 
         try(Socket socket = new Socket(SERVER_ADDRESS,SERVER_PORT);
-            OutputStream outputStream = socket.getOutputStream();)
-        {
-            PrintWriter out = new PrintWriter(outputStream,true);
-            System.out.println("The client is running and has connected to the server.");
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader console = new BufferedReader(new InputStreamReader(System.in)))
+        {
+            System.out.println("The client is running and has connected to the server.");
+
+            while(true)
+            {
+                //Display menu:
+                System.out.println("1. Display Product by ID");
+                System.out.println("2. Display Vendor by ID");
+                System.out.println("3. Display Vendors selling chosen Product");
+                System.out.println("4. Display Products sold by chosen Vendor");
+                System.out.println();
+                System.out.print("Choose an option: ");
+
+                String option = console.readLine();
+
+                //Send user's option to the server
+                out.println(option);
+
+                if(option.equals("1"))
+                {
+                    System.out.print("Enter ID: ");
+                    String id = console.readLine();
+                    //Send ID to the server
+                    out.println(id);
+                    String pString = in.readLine();
+                    System.out.println("Response from the server: " + pString);
+                }
+                else if(option.equals("2"))
+                {
+                    System.out.print("Enter ID: ");
+                    String id = console.readLine();
+                    out.println(id);
+                    String vString = in.readLine();
+                    System.out.println("Response from the server: " + vString);
+                }
+                else if(option.equals("3"))
+                {
+                    System.out.print("Enter ID: ");
+                    String id = console.readLine();
+                    out.println(id);
+                    String vPString = in.readLine();
+                    System.out.println("Response from the server: " + vPString);
+                }
+                else if(option.equals("4"))
+                {
+                    System.out.print("Enter ID: ");
+                    String id = console.readLine();
+                    out.println(id);
+                    String pVString = in.readLine();
+                    System.out.println("Response from the server: " + pVString);
+                }
+                else
+                {
+                    System.out.println("Invalid request or error in response");
+                    break;
+                }
+
+                String jsonResponse = in.readLine();
+                if(jsonResponse != null)
+                {
+                    System.out.println("Received JSON data: ");
+                    System.out.println(jsonResponse);
+                }
+
+                System.out.println();
+            }
+
         }
         catch (UnknownHostException e)
         {
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -7,6 +7,11 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import DTOs.Product;
+import DTOs.Vendor;
+import Exceptions.DaoException;
+import DAOs.*;
+
 public class Server
 {
     /**
@@ -17,33 +22,75 @@ public class Server
     {
         final int PORT = 8888; //port number to listen for
 
+        ProductDaoInterface IProductDao = new MySqlProductDao();
+        VendorDaoInterface IVendorDao = new MySqlVendorDao();
+        ProductsVendorsDaoInterface IProductsVendorsDao = new MySqlProductsVendorsDao();
+        JsonConverter jsonConverter = new JsonConverter();
+
+        int id;
+        String jsonString;
+
         try (ServerSocket serverSocket = new ServerSocket(PORT))
         {
-            System.out.println("Server started. Waiting for the client to connect...");
+            while (true)
+            {
+                try (   Socket clientSocket = serverSocket.accept();
+                        // connection is made.
+                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                )
+                {
+                    System.out.println("Server started. A client has connected.");
 
-            //Accept client connection
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Client connected: " + clientSocket);
+                    //Read client's request
+                    String option = in.readLine();
+                    System.out.println("Received option from client: " + option);
 
-            //Open streams for communication with the client
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-
-            //Read client's request
-            String request = in.readLine();
-            System.out.println("Received request from client: " + request);
-
-            //Process request (echo back to client)
-            out.println("Server received your request: " + request);
-
-            //Close streams and sockets
-            in.close();
-            out.close();
-            clientSocket.close();
+                    switch (option)
+                    {
+                        case "1":
+                            id = Integer.parseInt(in.readLine());
+                            System.out.println(id);
+                            out.println("Display Product by ID: ");
+                            jsonString = jsonConverter.ConvertProductToJsonString(IProductDao.getProductById(id));
+                            out.println(jsonString);
+                            break;
+                        case "2":
+                            id = Integer.parseInt(in.readLine());
+                            System.out.println(id);
+                            out.println("Display Vendor by ID: ");
+                            jsonString = jsonConverter.ConvertProductToJsonString(IVendorDao.getVendorById(id));
+                            out.println(jsonString);
+                            break;
+                        case "3":
+                            id = Integer.parseInt(in.readLine());
+                            System.out.println(id);
+                            out.println("Display Vendors selling chosen Product: ");
+                            jsonString = jsonConverter.convertVendorListToJsonString(IProductsVendorsDao.getVendorsSellingProductId(id));
+                            out.println(jsonString);
+                            break;
+                        case "4":
+                            id = Integer.parseInt(in.readLine());
+                            System.out.println(id);
+                            out.println("Display Products sold by chosen Vendor: ");
+                            jsonString = jsonConverter.ConvertProductToJsonString(IProductsVendorsDao.getProductsSoldByVendorId(id));
+                            out.println(jsonString);
+                            break;
+                        default:
+                            out.println("Invalid option");
+                            break;
+                    }
+                    out.flush();
+                }
+            }
         }
         catch (IOException e)
         {
             e.printStackTrace();
+        }
+        catch (DaoException e)
+        {
+            throw new RuntimeException(e);
         }
     }
 }
